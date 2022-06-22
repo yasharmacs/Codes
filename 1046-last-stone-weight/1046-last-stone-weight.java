64619b2a57cdddf8263cdf6b0630e028ec3ba8bd
Yash Sharma <40769775+yasharmacs@users.noreply.github.com>
2022-06-23 01:29:34 +0530

    Time: 1 ms (99.62%), Space: 40 MB (82.93%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,30 +1,51 @@
-class Solution {
+// class Solution {
+//     public int lastStoneWeight(int[] stones) {
+//         PriorityQueue<Integer> q = new PriorityQueue<Integer>(Collections.reverseOrder());
+//         for(int i = 0; i < stones.length; i++){
+// 		//Add elements to the priority queue
+//             q.add(stones[i]);
+//         }
+//         int i = q.peek();
+//         int j = i;
+//         while(!q.isEmpty()){
+//             //Get max element
+//             i = q.poll();
+//             if(q.isEmpty()){
+//                 break;
+//             }else{
+//                 // get second max element
+//                 j = q.poll();
+//             }
+//             // check if x != y
+//             if(i != j){
+//                 // y = y - x
+//                 i = i-j;
+//                 q.add(i);
+//             }else if(i == j && q.isEmpty()){
+//                 return 0;
+//             }
+//         }
+//         return i;
+//     }
+// }
+//NEW UPDATED CODE BY YASH SHARMA:
+
+class Solution{
     public int lastStoneWeight(int[] stones) {
-        PriorityQueue<Integer> q = new PriorityQueue<Integer>(Collections.reverseOrder());
-        for(int i = 0; i < stones.length; i++){
-		//Add elements to the priority queue
-            q.add(stones[i]);
+        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+        for(int stoned: stones){
+            pq.add(stoned);
         }
-        int i = q.peek();
-        int j = i;
-        while(!q.isEmpty()){
-            //Get max element
-            i = q.poll();
-            if(q.isEmpty()){
-                break;
-            }else{
-                // get second max element
-                j = q.poll();
+        while(pq.size()>1){
+            int a1=pq.poll();
+            int a2=pq.poll();
+            if(a1==a2){
+                continue;
             }
-            // check if x != y
-            if(i != j){
-                // y = y - x
-                i = i-j;
-                q.add(i);
-            }else if(i == j && q.isEmpty()){
-                return 0;
+            if(a1 != a2){
+                pq.add(Math.abs(a1-a2));
             }
         }
-        return i;
+        return pq.size()==0 ? 0:pq.poll();
     }
 }
