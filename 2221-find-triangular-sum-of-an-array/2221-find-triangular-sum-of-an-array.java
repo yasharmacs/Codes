5e9f3312609509811c3600346e595ffb5bbe3831
Yash Sharma <40769775+yasharmacs@users.noreply.github.com>
2022-09-08 23:55:55 +0530

    Time: 91 ms (73.17%), Space: 48.7 MB (45.28%) - LeetHub

--- 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.java
+++ 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.java
@@ -1,25 +1,11 @@
 class Solution {
-    public int triangularSum(int[] nums) {
-        List<Integer> n=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            n.add(nums[i]);
-        }
-        List<List<Integer>> fal=new ArrayList<>();
-        fal.add(n);
-        for(int i=0;i<n.size()-1;i++){
-            List<Integer> al=new ArrayList<>();
-            for(int j=0;j<fal.get(i).size()-1;j++){
-                al.add((fal.get(i).get(j)+fal.get(i).get(j+1))%10);
-            }
-            fal.add(al);
-        }
-        int z=fal.size();
-        // return fal.get(z).get(0);
-        // for(int i=0;i<z;i++){
-        //     fal
-        // }
-        // System.out.println("ANS");
-        // System.out.println(fal.get(4).get(0));
-        return fal.get(z-1).get(0);
+    public int triangularSum(int[] arr) {
+        int n=arr.length;
+     for(int i=0;i<n;i++){
+         for(int j=0;j<n-i-1;j++){
+             arr[j]=(arr[j]+arr[j+1])%10;
+         }
+     }   
+        return arr[0];
     }
 }
